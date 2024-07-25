f8ea177a6082e72a7f37078867f8a9d719654120
gauravverma285 <gaurav.verma@singhsoft.com>
2024-07-25 15:40:54 +0530

    rbl status is done

--- DSA/rbl_response.py
+++ DSA/rbl_response.py
@@ -1,4 +1,4 @@
-# for IMPS 
+# for IMPS ((((Initiated))))
 
 {
     "Single_Payment_Corp_Resp":{
@@ -24,3 +24,89 @@
                     "Signature":"Signature"
                     }
                     }}
+
+# for NEFT ((((In Progress))))
+
+{"Single_Payment_Corp_Resp":{
+    "Header":{
+        "TranID":"9519449150",
+        "Corp_ID":"SINGHTKLTD",
+        "Maker_ID":"M005",
+        "Checker_ID":"C003",
+        "Approver_ID":"A003",
+        "Status":"In Progress",
+        "Error_Cde":"","Error_Desc":""
+        },
+        "Signature":{
+            "Signature":"Signature"}}}
+
+# for NEFT ((((Initiated))))
+
+{"Single_Payment_Corp_Resp":{
+    "Header":{
+        "TranID":"1272004503",
+        "Corp_ID":"SINGHTKLTD",
+        "Maker_ID":"M005",
+        "Checker_ID":"C003",
+        "Approver_ID":"A003",
+        "Status":"Initiated",
+        "Error_Cde":{},
+        "Error_Desc":{}
+        },
+        "Body":{
+            "RefNo":"SPSINGHTKLTD1272004503",
+            "UTRNo":"RATNN24200329588",
+            "PONum":"000284357490",
+            "Ben_Acct_No":"109566016481",
+            "Amount":"200.01",
+            "BenIFSC":"DNSB0000021",
+            "Txn_Time":"2024-07-18 15:31:37.591324"
+            },
+            "Signature":{
+                "Signature":"Signature"}}}
+
+
+# for RTGS  ((((In Progress))))
+
+{"Single_Payment_Corp_Resp":{
+    "Header":{
+        "TranID":"2018893726",
+        "Corp_ID":"SINGHTKLTD",
+        "Maker_ID":"M005",
+        "Checker_ID":"C003",
+        "Approver_ID":"A003",
+        "Status":"In Progress",
+        "Error_Cde":"",
+        "Error_Desc":""
+        },
+        "Signature":{"Signature":"Signature"}}}
+
+
+# for RTGS  ((((Initiated))))
+
+{"Single_Payment_Corp_Resp":{
+    "Header":{
+        "TranID":"8797876441",
+        "Corp_ID":"SINGHTKLTD",
+        "Maker_ID":"M005",
+        "Checker_ID":"C003",
+        "Approver_ID":"A003",
+        "Status":"Initiated",
+        "Error_Cde":{},
+        "Error_Desc":{}
+        },
+        "Body":{
+            "RefNo":"SPSINGHTKLTD8797876441",
+            "UTRNo":"RATNH24200329590",
+            "PONum":"000284357492",
+            "Ben_Acct_No":"109566016481",
+            "Amount":"200000.01",
+            "BenIFSC":"DNSB0000021",
+            "Txn_Time":"2024-07-18 16:17:01.544118"
+            },
+            "Signature":{"Signature":"Signature"}}}
+
+
+
+# NEFT success ------> status fetched 
+# RTGS success ------> status fetched 
